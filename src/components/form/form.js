7ae1fbbfbbe8659250d1dc1f46ea15e190444e60
Tommi <tmmi.makela@outlook.com>
2022-02-14 14:12:25 +0200

    chore: Add validations

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -10,21 +10,29 @@ const Form = (props) => {
       <input
         value={props.firstname}
         onChange={({ target }) => props.setFirstname(target.value)}
+        required
+
       />
       <p>Lastname:</p>
       <input
         value={props.lastname}
         onChange={({ target }) => props.setLastname(target.value)}
+        required
+
       />
       <p>Email:</p>
       <input
         value={props.email}
         onChange={({ target }) => props.setEmail(target.value)}
+        required
+
       />
       <p>Phone:</p>
       <input
         value={props.phone}
         onChange={({ target }) => props.setPhone(target.value)}
+        required
+
       />
 
       <button type='submit' >Add</button>
