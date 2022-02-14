ed81fdba59185d8894edbfc05c41b973966db9c2
Tommi <tmmi.makela@outlook.com>
2022-02-14 10:26:00 +0200

    feat: Add a feature to show all employees

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+/**
+ * Module dependencies
+ */
+import React, { useEffect, useState } from 'react';
+import employeeService from './services/employee';
 
+/**
+ * Main component
+ */
 const App = () => {
+
+  /**
+   * Component states
+   */
+  const [employees, setEmployees] = useState([]);
+
+  /**
+   * Get all employees from the backend
+   */
+  useEffect(() => {
+
+    employeeService
+      .getAllEmployees().then(initialEmployees => {
+        setEmployees(initialEmployees);
+      });
+
+  }, []);
+
+  console.log('render', employees.length, 'employees');
+  console.log(employees);
 
   return (
     <div>
-      Hello world!
+
+      {employees.map(employee =>
+
+        <div key={employee.id} >
+          <h2> {employee.firstname} {employee.lastname} </h2>
+          <p>Email: {employee.email} </p>
+          <p>phone: {employee.phone} </p>
+        </div>
+
+      )}
+
     </div>
   );
 
